0d14077e5a4cebf8330c8f5d796dd2b931cdc9dd
shvhaver <Stef.VanHavermaet@UGent.be>
2019-11-01 20:15:23 +0100

    skeleton ilw controller

--- controllers/footbot_ilw/footbot_ilw.cpp
+++ controllers/footbot_ilw/footbot_ilw.cpp
@@ -3,7 +3,178 @@
 /****************************************/
 /****************************************/
 
-CFootBotIndividualLevyWalk::CFootBotIndividualLevyWalk() {}
+CFootBotIndividualLevyWalk::CFootBotIndividualLevyWalk():
+   m_pcWheels(NULL),
+   m_pcProximity(NULL),
+   m_pcRNG(NULL) {}
+
+/****************************************/
+/****************************************/
+
+void CFootBotIndividualLevyWalk::Init(TConfigurationNode& t_node) {
+   try {
+      m_pcWheels    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+      m_pcProximity = GetSensor  <CCI_FootBotProximitySensor      >("footbot_proximity"    );
+
+      m_sDiffusionParams.Init(GetNode(t_node, "diffusion"));
+      m_sWheelTurningParams.Init(GetNode(t_node, "wheel_turning"));
+   }
+   catch(CARGoSException& ex) {
+      THROW_ARGOSEXCEPTION_NESTED("ERROR CFootBotIndividualLevyWalk - Init \"" << GetId() << "\"", ex);
+   }
+
+   m_pcRNG = CRandom::CreateRNG("argos");
+   Reset();
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotIndividualLevyWalk::ControlStep() {
+   switch(m_sStateData.State) {
+      case SStateData::STATE_INITIALIZE: {
+         Initialize();
+         break;
+      }
+      case SStateData::STATE_WALK: {
+         Walk();
+         break;
+      }
+      case SStateData::STATE_ROTATE: {
+         Rotate();
+         break;
+      }
+      case SStateData::STATE_COLLISIONAVOIDANCE: {
+          AvoidCollision();
+      }
+      default: {
+         LOGERR << "ERROR CFootBotIndividualLevyWalk - Invalid State" << std::endl;
+      }
+   }
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotIndividualLevyWalk::Initialize() {
+
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotIndividualLevyWalk::Walk() {
+    
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotIndividualLevyWalk::Rotate() {
+
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotIndividualLevyWalk::AvoidCollision() {
+
+}
+
+/****************************************/
+/****************************************/
+
+CVector2 CFootBotForaging::DiffusionVector(bool& b_collision) {
+   const CCI_FootBotProximitySensor::TReadings& tProxReads = m_pcProximity->GetReadings();
+   CVector2 cDiffusionVector;
+   for(size_t i = 0; i < tProxReads.size(); ++i) {
+      cDiffusionVector += CVector2(tProxReads[i].Value, tProxReads[i].Angle);
+   }
+   /* If the angle of the vector is small enough and the closest obstacle
+      is far enough, ignore the vector and go straight, otherwise return
+      it */
+   if(m_sDiffusionParams.GoStraightAngleRange.WithinMinBoundIncludedMaxBoundIncluded(cDiffusionVector.Angle()) &&
+      cDiffusionVector.Length() < m_sDiffusionParams.Delta ) {
+      b_collision = false;
+      return CVector2::X;
+   }
+   else {
+      b_collision = true;
+      cDiffusionVector.Normalize();
+      return -cDiffusionVector;
+   }
+}
+
+/****************************************/
+/****************************************/
+
+void CFootBotForaging::SetWheelSpeedsFromVector(const CVector2& c_heading) {
+   CRadians cHeadingAngle = c_heading.Angle().SignedNormalize();
+   Real fHeadingLength = c_heading.Length();
+   Real fBaseAngularWheelSpeed = Min<Real>(fHeadingLength, m_sWheelTurningParams.MaxSpeed);
+   /* State transition logic */
+   if(m_sWheelTurningParams.TurningMechanism == SWheelTurningParams::HARD_TURN) {
+      if(Abs(cHeadingAngle) <= m_sWheelTurningParams.SoftTurnOnAngleThreshold) {
+         m_sWheelTurningParams.TurningMechanism = SWheelTurningParams::SOFT_TURN;
+      }
+   }
+   if(m_sWheelTurningParams.TurningMechanism == SWheelTurningParams::SOFT_TURN) {
+      if(Abs(cHeadingAngle) > m_sWheelTurningParams.HardTurnOnAngleThreshold) {
+         m_sWheelTurningParams.TurningMechanism = SWheelTurningParams::HARD_TURN;
+      }
+      else if(Abs(cHeadingAngle) <= m_sWheelTurningParams.NoTurnAngleThreshold) {
+         m_sWheelTurningParams.TurningMechanism = SWheelTurningParams::NO_TURN;
+      }
+   }
+   if(m_sWheelTurningParams.TurningMechanism == SWheelTurningParams::NO_TURN) {
+      if(Abs(cHeadingAngle) > m_sWheelTurningParams.HardTurnOnAngleThreshold) {
+         m_sWheelTurningParams.TurningMechanism = SWheelTurningParams::HARD_TURN;
+      }
+      else if(Abs(cHeadingAngle) > m_sWheelTurningParams.NoTurnAngleThreshold) {
+         m_sWheelTurningParams.TurningMechanism = SWheelTurningParams::SOFT_TURN;
+      }
+   }
+   /* Wheel speeds based on current turning state */
+   Real fSpeed1, fSpeed2;
+   switch(m_sWheelTurningParams.TurningMechanism) {
+      case SWheelTurningParams::NO_TURN: {
+         /* Just go straight */
+         fSpeed1 = fBaseAngularWheelSpeed;
+         fSpeed2 = fBaseAngularWheelSpeed;
+         break;
+      }
+      case SWheelTurningParams::SOFT_TURN: {
+         /* Both wheels go straight, but one is faster than the other */
+         Real fSpeedFactor = (m_sWheelTurningParams.HardTurnOnAngleThreshold - Abs(cHeadingAngle)) / m_sWheelTurningParams.HardTurnOnAngleThreshold;
+         fSpeed1 = fBaseAngularWheelSpeed - fBaseAngularWheelSpeed * (1.0 - fSpeedFactor);
+         fSpeed2 = fBaseAngularWheelSpeed + fBaseAngularWheelSpeed * (1.0 - fSpeedFactor);
+         break;
+      }
+      case SWheelTurningParams::HARD_TURN: {
+         /* Opposite wheel speeds */
+         fSpeed1 = -m_sWheelTurningParams.MaxSpeed;
+         fSpeed2 =  m_sWheelTurningParams.MaxSpeed;
+         break;
+      }
+   }
+   /* Apply the calculated speeds to the appropriate wheels */
+   Real fLeftWheelSpeed, fRightWheelSpeed;
+   if(cHeadingAngle > CRadians::ZERO) {
+      /* Turn Left */
+      fLeftWheelSpeed  = fSpeed1;
+      fRightWheelSpeed = fSpeed2;
+   }
+   else {
+      /* Turn Right */
+      fLeftWheelSpeed  = fSpeed2;
+      fRightWheelSpeed = fSpeed1;
+   }
+   /* Finally, set the wheel speeds */
+   m_pcWheels->SetLinearVelocity(fLeftWheelSpeed, fRightWheelSpeed);
+}
+
+/****************************************/
+/****************************************/
 
 /****************************************/
 /****************************************/
--- controllers/footbot_ilw/footbot_ilw.h
+++ controllers/footbot_ilw/footbot_ilw.h
@@ -5,30 +5,79 @@
  *
  */
 
- #ifndef FOOTBOT_ILW_H
- #define FOOTBOT_ILW_H
+#ifndef FOOTBOT_ILW_H
+#define FOOTBOT_ILW_H
+
+#include <argos3/core/control_interface/ci_controller.h>
+#include <argos3/plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
+#include <argos3/plugins/robots/foot-bot/control_interface/ci_footbot_proximity_sensor.h>
 
 using namespace argos;
 
-class CFootBotIndividualLevyWalk : public CCI_Controller {
-public:
-  struct SStateData {
-     enum EState {
-        STATE_INITIALIZE = 0,
-        STATE_WALK,
-        STATE_ROTATE,
-        STATE_COLLISIONAVOIDANCE
-     } State;
+class CFootBotIndividualLevyWalk : public CCI_Controller
+{
 
-     SStateData();
-     void Init(TConfigurationNode& t_node);
-     void Reset();
-   };
+public:
+  struct SStateData
+  {
+    enum EState
+    {
+      STATE_INITIALIZE = 0,
+      STATE_WALK,
+      STATE_ROTATE,
+      STATE_COLLISIONAVOIDANCE
+    } State;
+
+    SStateData();
+    void Reset();
+  };
+  struct SDiffusionParams
+  {
+    Real Delta;
+    CRange<CRadians> GoStraightAngleRange;
+
+    void Init(TConfigurationNode &t_tree);
+  };
+  struct SWheelTurningParams
+  {
+    enum ETurningMechanism
+    {
+      NO_TURN = 0, // go straight
+      SOFT_TURN,   // both wheels are turning forwards, but at different speeds
+      HARD_TURN    // wheels are turning with opposite speeds
+    } TurningMechanism;
+
+    CRadians HardTurnOnAngleThreshold;
+    CRadians SoftTurnOnAngleThreshold;
+    CRadians NoTurnAngleThreshold;
+
+    Real MaxSpeed;
+
+    void Init(TConfigurationNode &t_tree);
+  };
+
 public:
   CFootBotIndividualLevyWalk();
   virtual ~CFootBotIndividualLevyWalk() {}
 
-  virtual void Init(TConfigurationNode& t_node);
+  virtual void Init(TConfigurationNode &t_node);
+  virtual void ControlStep();
+
+private:
+  CCI_DifferentialSteeringActuator *m_pcWheels;
+  CCI_FootBotProximitySensor *m_pcProximity;
+
+  SStateData m_sStateData;
+  SDiffusionParams m_sDiffusionParams;
+  SWheelTurningParams m_sWheelTurningParams;
+
+  CRandom::CRNG *m_pcRNG;
+
+private:
+  void Initialize();
+  void Walk();
+  void Rotate();
+  void AvoidCollision();
 };
 
 #endif
