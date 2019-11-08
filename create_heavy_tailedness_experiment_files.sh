1b63f13435dd109ec67c51c029d9ecbb144ff9c6
jnauta <jnauta@node0.quick16.wall2-ilabt-iminds-be.wall1.ilabt.iminds.be>
2019-11-08 10:04:15 -0700

    Updates to saving data on the shared directory

--- create_heavy_tailedness_experiment_files.sh
+++ create_heavy_tailedness_experiment_files.sh
@@ -8,9 +8,9 @@ T=$1      # Time length of experiment
 N=$2      # Sequence of swarm sizes
 seeds=$3  # Sequence of seeds to re-run single experiment
 
-# Params default (file-related):
+# Directory parameters:
 experiment_upper_folder="experiments"
-results_upper_folder="results"
+results_upper_folder="/groups/wall2-ilabt-iminds-be/jnauta/exp/collective_levy/CollectiveLevyWalk/results"
 title="heavy_tailedness"
 step_lengths_title="step_lengths"
 state_counters_title="state_counters"
@@ -24,6 +24,7 @@ save_step_lengths="true"
 
 # Execute:
 for robot_controller in "${robot_controllers[@]}"; do
+  # Remove all 
   for swarm_size in $N; do
     for seed in $seeds; do
       folder="${title}/${robot_controller}/${swarm_size}N/${seed}"
@@ -31,8 +32,9 @@ for robot_controller in "${robot_controllers[@]}"; do
       mkdir -p "${experiment_upper_folder}/${folder}"
       mkdir -p "${results_upper_folder}/${folder}"
 
-      experiment_title="${title}_${robot_controller}_${swarm_size}N_${seed}"
+      experiment_title="${title}"
       experiment_file="${experiment_upper_folder}/${folder}/${experiment_title}.argos"
+
       # Copy the base .argos file
       cp "${base_experiment_file}" "${experiment_file}"
 
@@ -42,7 +44,7 @@ for robot_controller in "${robot_controllers[@]}"; do
       xmlstarlet ed --pf --inplace -u 'argos-configuration//framework/experiment/@length' -v "${T}" "${experiment_file}"
       xmlstarlet ed --pf --inplace -u 'argos-configuration//loop_functions/swarm/@swarm_size' -v "${swarm_size}" "${experiment_file}"
       xmlstarlet ed --pf --inplace -u 'argos-configuration//loop_functions/swarm/@nested' -v "${nested}" "${experiment_file}"
-      xmlstarlet ed --pf --inplace -u 'argos-configuration//loop_functions/output/@output_file' -v "${results_upper_folder}\/${folder}\/${experiment_title}" "${experiment_file}"
+      xmlstarlet ed --pf --inplace -u 'argos-configuration//loop_functions/output/@output_file' -v "${results_upper_folder}/${folder}/${experiment_title}" "${experiment_file}"
       xmlstarlet ed --pf --inplace -u 'argos-configuration//loop_functions/output/@save_step_lenghts' -v "${save_step_lenghts}" "${experiment_file}"
 
       # sed -i "s/\(robot_controller=\"\)[^\"]*\"/\1${robot_controller}\"/" "${experiment_file}"
