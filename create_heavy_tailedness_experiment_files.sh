1bdc73af946044913c35e15c2ddea20b5195a90f
Stef Van Havermaet <stef.vanhavermaet@ugent.be>
2019-11-08 19:35:40 +0100

    search efficiency script

--- create_heavy_tailedness_experiment_files.sh
+++ create_heavy_tailedness_experiment_files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Creates the required argos experiment file(s)
+# Creates the required argos experiment files and result directories
 # to analyse the heavy tailedness of the collective trajectory
 # executed by a swarm existing of either ilw or clw robots
 
-# Params from args:
+# Args:
 T=$1      # Time length of experiment
 N=$2      # Sequence of swarm sizes
 seeds=$3  # Sequence of seeds to re-run single experiment
@@ -12,14 +12,11 @@ seeds=$3  # Sequence of seeds to re-run single experiment
 experiment_upper_folder="experiments"
 results_upper_folder="/groups/wall2-ilabt-iminds-be/jnauta/exp/collective_levy/CollectiveLevyWalk/results"
 title="heavy_tailedness"
-step_lengths_title="step_lengths"
-state_counters_title="state_counters"
 base_experiment_file="${experiment_upper_folder}/base/base.argos"
 
-# Params default (experiment-related):
+# Experiment parameters:
 robot_controllers=(ilw clw)
 nested="false"
-target_positions_file=""
 save_step_lengths="true"
 
 # Execute:
@@ -28,7 +25,6 @@ for robot_controller in "${robot_controllers[@]}"; do
   for swarm_size in $N; do
     for seed in $seeds; do
       folder="${title}/${robot_controller}/${swarm_size}N/${seed}"
-      folder_escaped="${title}\/${robot_controller}\/${swarm_size}N\/${seed}"
       mkdir -p "${experiment_upper_folder}/${folder}"
       mkdir -p "${results_upper_folder}/${folder}"
 
@@ -46,13 +42,6 @@ for robot_controller in "${robot_controllers[@]}"; do
       xmlstarlet ed --pf --inplace -u 'argos-configuration//loop_functions/swarm/@nested' -v "${nested}" "${experiment_file}"
       xmlstarlet ed --pf --inplace -u 'argos-configuration//loop_functions/output/@output_file' -v "${results_upper_folder}/${folder}/${experiment_title}" "${experiment_file}"
       xmlstarlet ed --pf --inplace -u 'argos-configuration//loop_functions/output/@save_step_lenghts' -v "${save_step_lenghts}" "${experiment_file}"
-
-      # sed -i "s/\(robot_controller=\"\)[^\"]*\"/\1${robot_controller}\"/" "${experiment_file}"
-      # sed -i "s/\(swarm_size=\"\)[^\"]*\"/\1${swarm_size}\"/" "${experiment_file}"
-      # sed -i "s/\(nested=\"\)[^\"]*\"/\1${nested}\"/" "${experiment_file}"
-      # sed -i "s/\(target_positions_file=\"\)[^\"]*\"/\1${target_positions_file}\"/" "${experiment_file}"
-      # sed -i "s/\(output_file=\"\)[^\"]*\"/\1${results_upper_folder}\/${folder_escaped}\/${experiment_title}\"/" "${experiment_file}"
-      # sed -i "s/\(save_step_lengths=\"\)[^\"]*\"/\1${save_step_lengths}\"/" "${experiment_file}"
     done
   done 
 done
